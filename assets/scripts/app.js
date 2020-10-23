21174de223142a037158872240a91d2912203dd7
FuiOcloo <fui.ocloo@amalitech.org>
2020-10-23 22:44:58 +0000

    functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,14 @@
 const defaultNumber = 0;
 let currentNumber = defaultNumber;
 
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert("The result is " + result);
+}
+
+add(3,2);
+add(5, 10);
+
 currentNumber = (currentNumber + 10) * 3 / 2 - 1;
 
 let calculationDescription = '(' + defaultNumber + '+ 10) * 3/ 2 -1';
