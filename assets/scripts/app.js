375f4b1a27f4c42b38b1d006cfe1e019c60bd080
FuiOcloo <fui.ocloo@amalitech.org>
2020-10-23 13:56:22 +0000

    variables

---
+++ assets/scripts/app.js
@@ -0,0 +1,8 @@
+const defaultNumber = 0;
+let currentNumber = defaultNumber;
+
+currentNumber = (currentNumber + 10) * 3 / 2 - 1;
+
+let calculationDescription = "(" + defaultNumber + "+ 10) * 3/ 2 -1";
+
+outputResult(currentNumber, calculationDescription);
