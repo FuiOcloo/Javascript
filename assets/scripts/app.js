919ace648e6cb8e2d6176394d04bfe5eff99882c
FuiOcloo <fui.ocloo@amalitech.org>
2020-10-23 13:57:55 +0000

    variable types

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,6 @@ let currentNumber = defaultNumber;
 
 currentNumber = (currentNumber + 10) * 3 / 2 - 1;
 
-let calculationDescription = "(" + defaultNumber + "+ 10) * 3/ 2 -1";
+let calculationDescription = '(' + defaultNumber + '+ 10) * 3/ 2 -1';
 
 outputResult(currentNumber, calculationDescription);
