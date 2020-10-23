305ab2d37890f1e91db529b4a111271f2796fe73
FuiOcloo <fui.ocloo@amalitech.org>
2020-10-23 23:03:08 +0000

    returning value

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,13 +3,11 @@ let currentNumber = defaultNumber;
 
 function add(num1, num2) {
     const result = num1 + num2;
-    alert("The result is " + result);
+    return result;
 }
 
-add(3,2);
-add(5, 10);
+currentNumber = add(2,2);
 
-currentNumber = (currentNumber + 10) * 3 / 2 - 1;
 
 let calculationDescription = '(' + defaultNumber + '+ 10) * 3/ 2 -1';
 
