0437bf2cd82e995968f56f4dfd0285ba8aeb9568
FuiOcloo <fui.ocloo@amalitech.org>
2020-10-24 23:35:59 +0000

    connecting buttons to functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,11 +5,40 @@ function getUserNumberInput(){
     return parseInt(userInput.value);
 }
 
-function add() {
-    const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentNumber} + ${enteredNumber}`;
-    currentNumber = currentNumber + enteredNumber;
+function createAndWriteOutput(operator, resultBeforeCalc, CalcNumber){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${CalcNumber}`;
     outputResult(currentNumber, calcDescription);
 }
 
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentNumber;
+    currentNumber = currentNumber + enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtract(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentNumber;
+    currentNumber = currentNumber - enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentNumber;
+    currentNumber = currentNumber * enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentNumber;
+    currentNumber = currentNumber / enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
